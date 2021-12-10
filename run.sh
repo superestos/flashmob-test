09b146e4410d38a0cdf9a14ce81c956c1041d2ea
superestos <superestos@gmail.com>
2021-12-10 07:42:26 +0000

    new dataset path

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-dataset=~/dataset/soc-LiveJournal1.el
+dataset=~/dataset/bel/soc-LiveJournal1.bel
 walkers=100000000
 length=10
 
-build/bin/deepwalk -f text -g  $dataset -w $walkers -l $length
+build/bin/deepwalk -f binary -g  $dataset -w $walkers -l $length
